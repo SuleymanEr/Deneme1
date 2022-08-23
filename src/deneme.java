cf991eae3e4bb0777ce30882c7cead7cbf361702
SuleymanEr <111180776+SuleymanEr@users.noreply.github.com>
2022-08-23 12:08:52 +0200

    Update deneme.java

--- src/deneme.java
+++ src/deneme.java
@@ -1,6 +1,8 @@
 public class deneme {
     public static void main(String[] args) {
         System.out.println("helle worldddddd");
+        System.out.println("helle worldddddd333");
+           
     }
 
 }
